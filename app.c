6be1991d9c3392479f812bf02569fbce623d5d70
Yohendev <siberneitor.vitor540lb@gmail.com>
2025-04-15 10:03:25 -0300

    Apps c 1

---
+++ app.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <locale>
+#define POL 2.54
+
+float x, y, r;
+
+int main (){
+    setlocale(LC_ALL, "");
+	printf("Digite um numero inteiro: ");
+	scanf("%f", &x);
+	printf("valor em cm é %.2f \nO valor em pol é %.3f ",x,x/POL);
+	return 0;
+}
