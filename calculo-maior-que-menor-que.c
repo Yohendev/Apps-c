074cf738a0fdeea76fb79113ed0c1aaeb9b75bbd
Yohendev <siberneitor.vitor540lb@gmail.com>
2025-04-29 09:57:46 -0300

    Calculo maior menor quee

--- calculo-maior-que-menor-que.c
+++ calculo-maior-que-menor-que.c
@@ -2,28 +2,27 @@
 #include <locale.h>
 
 int main (){
-    int valor;
+    int A, B;
 
     setlocale(LC_ALL,"");
-    printf ("Digite um numero inteiro: ");
+
+    printf("Digite um número inteiro: ");
     scanf("%i", &A);
-    printf("Digite outro número inteiro");
+
+    printf("Digite outro número inteiro: ");
     scanf("%i", &B);
 
-    if (A==B)
-    {
-        printf("A - B - %d", A);
-    }
-    else {
-        printf("os valores são diferentes!");
+    if (A == B) {
+        printf("A - B - %d\n", A);
+    } else {
+        printf("Os valores são diferentes!\n");
 
-        if(A>B){
-            printf("A(%i) > B(%i)", A,B);
-        }
-        else{
-            printf("A(%i) < B(%i)", A,B);
+        if (A > B) {
+            printf("A(%i) > B(%i)\n", A, B);
+        } else {
+            printf("A(%i) < B(%i)\n", A, B);
         }
     }
 
-   return 0;
+    return 0;
 }
