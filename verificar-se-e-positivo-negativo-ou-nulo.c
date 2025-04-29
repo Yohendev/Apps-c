91d9f5b9793c30e5afdbf896e6e02f86df199a09
Yohendev <siberneitor.vitor540lb@gmail.com>
2025-04-29 09:35:34 -0300

    Verificar se é positivo ou negativo

--- verificar-se-e-positivo-negativo-ou-nulo.c
+++ verificar-se-e-positivo-negativo-ou-nulo.c
@@ -4,7 +4,7 @@
 int main (){
     int valor;
 
-    setlocale(LC_ALL,"")
+    setlocale(LC_ALL,"");
     printf ("Digite um numero inteiro: ");
     scanf("%i", &valor);
 
@@ -12,6 +12,10 @@ int main (){
         printf("O valor da variavel é nulo.");
     }
     else if (valor > 0){
-
+    printf("O valor %i é positivo", valor);
     }
+    else{
+        printf("O valor %d é negativo", valor);
+    }
+    return 0;
 }
